fec3c2e351d9b61e37615acc519a85abd0581879
Ruben Soares Lapa Remelgado <rus14jh@uni-wuerzburg.eu>
2017-06-22 15:26:45 +0200

    Update

--- R/dataQuery.R
+++ R/dataQuery.R
@@ -4,20 +4,22 @@
 #' @param x Vector of x coordinates.
 #' @param y Vector of y coordinates.
 #' @param var Object of class RasterStack or RasterBrick containing environmental layers.
-#' @import raster, grDevices
+#' @import raster grDevices
 #' @return Matrix of environmental variables for each sample. If a sample does not overlap NA is returned.
 #' @examples \dontrun{
 #' }
+#' @export
 
 #-------------------------------------------------------------------------------------------------------------------------------#
 
 dataQuery <- function(x=x, y=y, var=var) {
   
   # check variables
-  if (!exists(xy)) {stop('error: "xy" is missing')}
+  if (!exists(x)) {stop('error: "x" is missing')}
+  if (!exists(y)) {stop('error: "y" is missing')}
   if (!exists(var)) {stop('error: "var" is missing')}
-  if (class(var)!='raster') {stop('error: "var" is not a valid raster object')}
-  if (nlength(x)!=length(y)) {stop('error: x and y have different lengths')}
+  if (class(var)!='RasterStack' & class(var)!='RasterBrick') {stop('error: "var" is not a valid raster object')}
+  if (length(x)!=length(y)) {stop('error: x and y have different lengths')}
   
   # derive pixel coordinates
   ext <- raster::extent(var) # raster extent
@@ -33,7 +35,7 @@ dataQuery <- function(x=x, y=y, var=var) {
   ind1 <- which(sp > 0 | sp < np) # pixels inside the valid range
   
   # retrieve/return environmental data
-  ov <- matrix(0, length(x), nlayers(var))
+  ov <- matrix(0, length(x), raster::nlayers(var))
   ov[ind0,] <- NA
   ov[ind1,] <- var[sp[ind1]]
   return(ov)
