baebc40dcee0d28fc87dda04d202508bb9637324
Ruben Soares Lapa Remelgado <rus14jh@uni-wuerzburg.eu>
2017-06-28 18:11:52 +0200

    Function update

--- R/poly2sample.R
+++ R/poly2sample.R
@@ -2,8 +2,8 @@
 #'
 #' @description Convert spatial polygons into x and y coordinates.
 #' @param pol Object of class "SpatialPolygons" or "SpatialPolygonDataFrame".
-#' @param ras Object of class "Raster".
-#' @param mpc Minimum pixel cover (0-100). Percent proportion a pixel should be covered by a polygon so that that sample is kept. Default is 1%.
+#' @param ras Object of class "Raster", "RasterBrick" or "RasterStack".
+#' @param mpc Minimum pixel cover (0-100). Percent proportion a pixel should be covered by a polygon so that that sample is kept. Default is 0%.
 #' @import raster grDevices
 #' @return Matrix containing unique samples. The output reports on sample coordinates ("x" and "y"), polygon id ("id") and pixel % cover ("cover").
 #' @examples \dontrun{
@@ -12,10 +12,12 @@
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------#
 
-poly2sample(pol, ras, mpc=1) {
+poly2sample(pol, ras, mpc=0) {
   
-  if(!class(pol)[1]%in%c('SpatialPolygons', 'SpatialPolygonDataFrame')) {}
-  
+  # check input
+  if(!class(pol)[1]%in%c('SpatialPolygons', 'SpatialPolygonDataFrame')) {stop('error: "pol" is not a valid input.')}
+  if(!class(ras)[1]%in%c("Raster", "RasterStack", "RasterBrick")) {stop('error: "ras" is not a valid input.')}
+  if(mpc>100 | mpc<0) {stop('error: "mpc" should be between 0 and 100.')}
   
   # reference raster specifications
   rr <- raster::raster(ras) # raster
@@ -36,13 +38,16 @@ poly2sample(pol, ras, mpc=1) {
     tr <- crop(rr, te) # target raster
     r = as.matrix(raster::rasterize(pol[1,], tr, getCover=T)) # rasterize polygon
     ind <- which (r > mpc) # identify usable pixels
-    xc[[p]] <- ((ind / nr)*pr) - re[1] # convert pixel coordinates to x
-    yc[[p]] <- re[2] - ((ind %% nr)*pr) # convert pixel coordinates to y
-    id[[p]] <- replicate(length(ind), p) # assign polygon ID ot coordinates
-    pc[[p]] <- r[ind] # assign pixel cover information
+    if (length(ind)>0) {
+      xc[[p]] <- ((ind / nr)*pr) - re[1] # convert pixel coordinates to x
+      yc[[p]] <- re[2] - ((ind %% nr)*pr) # convert pixel coordinates to y
+      id[[p]] <- replicate(length(ind), p) # assign polygon ID ot coordinates
+      pc[[p]] <- r[ind] # assign pixel cover information
+    }
+    rm(te, tr, r, ind)
   }
   
-  # build /return
+  # build /return data frame
   df <- data.frame(x=unlist(xc), y=unlist(yc), id=unlist(id), cover=pc, stringsAsFactors=F)
   return(df)
   
