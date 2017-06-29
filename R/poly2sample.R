868fc13b7f2054930f127b8c6b308ad08c81952a
Ruben Soares Lapa Remelgado <rus14jh@uni-wuerzburg.eu>
2017-06-29 11:59:14 +0200

    Function fix

--- R/poly2sample.R
+++ R/poly2sample.R
@@ -1,116 +1,100 @@
 #' @title poly2sample
 #'
-#' @description Convert spatial polygons into x and y coordinates.
+#' @description Convert spatial polygons into point samples.
 #' @param pol Object of class "SpatialPolygons" or "SpatialPolygonDataFrame".
-#' @param ras Object of class "Raster", "RasterBrick" or "RasterStack".
+#' @param re Object of class "Extent" or a raster object from which an extent can be derived.
 #' @param mpc Minimum pixel cover (0-100).
-#' @param pr Numeric value giving the pixel resolution.
+#' @param pr Pixel resolution.
 #' @param rp A valid "CRS" object.
-#' @param ccheck Logical. Shound the function check for duplicated pixels? Default is FALSE.
 #' @import raster grDevices
 #' @return Matrix containing unique samples.
-#' @details {Determines coordinates of pixels within a given extent or reference raster layer.
+#' @details {Determines coordinates of pixels within a given extent. If "re" is missing the
+#'            target extent corresponds to the extent of the polygon layer. In this case, "pr"
+#'            is required. If "re" is an "Extent" object or "re" is missing rp" is required. 
 #'            "mpc" can be used to filter pixels with low purity, i.e. pixels where the 
-#'            percentage of area cover by a polygon is below the defined threshold.
-#'            If an extent object is provided, a reference projection system ("rp") 
-#'            and a target resoluton ("pr") are required. The output reports on sample 
-#'            coordinates ("x" and "y"), polygon id ("id") and pixel percent cover ("cover").
-#'            If "ccheck" is TRUE the function checks if pixels were overlapped by multiple polygons.
-#'            If so, the code updates the percent cover estimations. This might be slow.}
+#'            percentage of area cover by a polygon is below the defined threshold. 
+#'            The output provides the coordinates ("x" and "y") the 
+#'            pixel percent cover ("cover") for each selected pixel.}
 #' @examples \dontrun{
 #' }
 #' @export
 
-#----------------------------------------------------------------------------------------------------------#
+#-------------------------------------------------------------------------------------------------------------------------#
 
-poly2sample(pol=pol, ras=ras, mpc=0, pr=NULL, rp=NULL, ccheck=F) {
+poly2sample(pol=pol, re=NULL, mpc=0, pr=NULL) {
   
   # check input
-  if(!exists('pol')) {stop('error: "pol" is missing')}
-  if(!exists('ras')) {stop('error: "ras" is missing')}
-  if(!class(pol)[1]%in%c('SpatialPolygons', 'SpatialPolygonDataFrame')) {
+  if(is.null('pol')) {stop('error: "pol" is missing')}
+  if(!class(pol)[1]%in%c('SpatialPolygons', 'SpatialPolygonsDataFrame')) {
     stop('error: "pol" is not a valid input.')
   }
-  if(!class(ras)[1]%in%c("Raster", "RasterStack", "RasterBrick")) {
-    if (class(ras)!='Extent') {stop('error: "ras" is not a valid input.')}
-    if (is.null(pr)) {stop('error: "ras" is of class "Extent" object. "pr" is required.')}
-    if (is.null(rp)) {stop('error: "ras" is of class "Extent" object. "rp" is required.')}
-    rr <- raster::raster(ras, res=pr, crs=rp) # create reference raster
+  if (!is.null(re)) {
+    if(!class(re)[1]%in%c("RasterLayer", "RasterStack", "RasterBrick")) {
+      if (class(re)!='Extent') {stop('error: "ras" is not a valid input.')}
+      if (is.null(pr)) {stop('error: "re" is of class "Extent" object. "pr" is required.')}
+      if (is.null(rp)) {stop('error: "re" is of class "Extent" object. "rp" is required.')}
+      rr <- round((re[4]-re[3]) / pr) + 1 # number of rows in raster
+    } else {
+      pr <- raster::res(re)[1] # check pixel resolution
+      rp <- raster::crs(re) # check raster projection
+      rr <- dim(re)[1] # number of rows in raster
+      re <- raster::extent(re) # extraxt extent
+    }
+    if (rp@projargs!=raster::crs(pol)@projargs) {stop('error: using different projections')}
   } else {
-    rr <- raster::raster(ras) # read reference raster
-    pr <- raster::res(ras) # check pixel resolution
-    rp <- raster::crs(ras) # check raster projection
-    ras <- raster::extent(rr) # extraxt extent
+    if (is.null(pr)) {stop('error: provide "pr" since "re" is missing')}
+    re <- raster::extent(pol) # extent derived from 
+    rr <- round((re[4]-re[3]) / pr) + 1 # number of rows in raster
   }
-  if (rp@projargs!=raster::crs(pol)@projargs) {stop('error: using different projections')}
   if (!exists('mpc')) {mpc <- 0}
   if (mpc>100 | mpc<0) {stop('error: "mpc" should be between 0 and 100.')}
 
-#----------------------------------------------------------------------------------------------------------#
+#-------------------------------------------------------------------------------------------------------------------------#
     
-  # check each polygon
-  xc <- list()
-  yc <- list()
-  id <- list()
-  pc <- list()
+  # function to apply
   lf <- function(i) {
     te <- raster::extent(pol[i,]) # target extent
-    tr <- raster::crop(rr, te) # target raster
-    r = as.matrix(raster::rasterize(pol[i,], tr, getCover=T)) # rasterize polygon
+    nc <- (round((te[2]-re[1])/pr)+1)-(round((te[1]-re[1])/pr)+1) # number of columns
+    nr <- (round((re[4]-te[3])/pr)+1)-(round((re[4]-te[4])/pr)+1) # number of rows
+    r <-raster(nrows=nr, ncols=nc, xmn=te[1], xmx=te[2], ymn=te[3], ymx=te[4], crs=rp, resolution=pr) # target raster
+    r = as.matrix(raster::rasterize(pol[i,], r, getCover=T)) # rasterize polygon
     nr <- dim(r)[1] # number of columns
     ind <- which (r > 0) # identify usable pixels
-    xc[[i]] <<- te[1]+((ind / nr)*pr) # x coordinates
-    yc[[i]] <<- te[4]-((ind %% nr)*pr) # y coordinates
-    id[[i]] <<- replicate(length(ind), i) # polygon ids
-    pc[[i]] <<- r[ind] # pixel percent cover
-    return()
+    xp <- round(((te[1]+((ind/nr)*pr))-ras[1])/pr)+1 # x image coordinates
+    yp <- round((ras[4]-(te[4]-((ind%%nr)*pr)))/pr)+1 # y image coordinates
+    return(list(pp=(yp+rr*xp), pc=r[ind])) # return metrics
   }
-  x <- sapply(1:np, lf)
-  xc <- unlist(xc)
-  yc <- unlist(yc)
-  id <- unlist(id)
-  pc <- unlist(pc)
+  np <- length(pol) # number of polygons
+  x <- lapply(1:np, lf) # evaluate polygons
+  sp <- unlist(sapply(x, function(x) {x$pp})) # pixel positions in raster
+  pc <- unlist(sapply(x, function(x) {x$pc})) # pixel percent cover
   
-  # convert coordinates to pixel indices
-  sp <- (round((ras[2]-yc)/pr)+1) + dim(rr)[1] * round((xc-ras[1])/pr)
+  rm(x)
   
-  rm(x, rr)
+#-------------------------------------------------------------------------------------------------------------------------#
   
-#----------------------------------------------------------------------------------------------------------#
+  # remove duplicated pixels and update pecent count
+  up <- unique(sp) # unique pixels
+  pc <- sapply(up, function(x) {sum(pc[which(sp==x)])}) # update percentages
+  pc[which(pc>100)] <- 100 # account for miss-calculations (related to e.g. overlapping polygons)
   
-  if (ccheck) {
-    
-    # remove duplicates (use unique pixel indices as reference)
-    # if duplicates exist, sum percent cover over repeated records
-    ui <- duplicated(sp) # which are the dupllicates?
-    ud <- unique(sp[ui]) # unique duplicates
-    ui <- !ui# records to keep
-    lf <- function (i) {
-      pc[[which(ui==T & sp==i)]] <<- sum(pc[which(sp==i)])
-      return()
-    }
-    x <- sapply(ud, lf)
-    xc <- xc[ui]
-    yc <- yc[ui]
-    sp <- sp[ui]
-    id <- id[ui]
-    pc <- pc[ui]
-    
-    rm(x, ud, ui)
-    
-  }
+  rm(sp)
   
-#----------------------------------------------------------------------------------------------------------#
+#-------------------------------------------------------------------------------------------------------------------------#
   
   # apply percent cover filter  
-  ind <- which(pc > mpc)
-  xc <- xc[ind]
-  yc <- yc[ind]
-  sp <- sp[ind]
-  id <- id[ind]
+  ind <- which(pc >= mpc)
+  up <- up[ind]
   pc <- pc[ind]
   
+#------------------------------------------------------------------------------------------------------------------------#
+  
   # build/return output
-  return(data.frame(x=xc, y=yc, index=sp, id=id, cover=pc))
+  xc <- re[1] + ((up/rr)*pr) # convert positions to x coordinates
+  yc <- re[4] - ((up%%rr)*pr) # convert positions to y coordinates
+  
+  return(data.frame(x=xc, y=yc, index=up, cover=pc))
+  
+#------------------------------------------------------------------------------------------------------------------------#
   
 }
