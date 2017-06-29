6c2ca699134b97a26b08848bb8f69fa880575de4
Ruben Soares Lapa Remelgado <rus14jh@uni-wuerzburg.eu>
2017-06-29 09:26:56 +0200

    function fix

--- R/poly2sample.R
+++ R/poly2sample.R
@@ -6,6 +6,7 @@
 #' @param mpc Minimum pixel cover (0-100).
 #' @param pr Numeric value giving the pixel resolution.
 #' @param rp A valid "CRS" object.
+#' @param ccheck Logical. Shound the function check for duplicated pixels? Default is FALSE.
 #' @import raster grDevices
 #' @return Matrix containing unique samples.
 #' @details {Determines coordinates of pixels within a given extent or reference raster layer.
@@ -13,14 +14,16 @@
 #'            percentage of area cover by a polygon is below the defined threshold.
 #'            If an extent object is provided, a reference projection system ("rp") 
 #'            and a target resoluton ("pr") are required. The output reports on sample 
-#'            coordinates ("x" and "y"), polygon id ("id") and pixel percent cover ("cover").}
+#'            coordinates ("x" and "y"), polygon id ("id") and pixel percent cover ("cover").
+#'            If "ccheck" is TRUE the function checks if pixels were overlapped by multiple polygons.
+#'            If so, the code updates the percent cover estimations. This might be slow.}
 #' @examples \dontrun{
 #' }
 #' @export
 
 #----------------------------------------------------------------------------------------------------------#
 
-poly2sample(pol=pol, ras=ras, mpc=0, pr=NULL, rp=NULL) {
+poly2sample(pol=pol, ras=ras, mpc=0, pr=NULL, rp=NULL, ccheck=F) {
   
   # check input
   if(!exists('pol')) {stop('error: "pol" is missing')}
@@ -45,53 +48,69 @@ poly2sample(pol=pol, ras=ras, mpc=0, pr=NULL, rp=NULL) {
 
 #----------------------------------------------------------------------------------------------------------#
     
-  # function to evaluate polygons
+  # check each polygon
+  xc <- list()
+  yc <- list()
+  id <- list()
+  pc <- list()
   lf <- function(i) {
     te <- raster::extent(pol[i,]) # target extent
-    tr <- crop(rr, te) # target raster
+    tr <- raster::crop(rr, te) # target raster
     r = as.matrix(raster::rasterize(pol[i,], tr, getCover=T)) # rasterize polygon
     nr <- dim(r)[1] # number of columns
     ind <- which (r > 0) # identify usable pixels
-    list(x=te[1]+((ind / nr)*pr), y=te[4]-((ind %% nr)*pr), 
-         id=replicate(length(ind), i), cover=r[ind])
+    xc[[i]] <<- te[1]+((ind / nr)*pr) # x coordinates
+    yc[[i]] <<- te[4]-((ind %% nr)*pr) # y coordinates
+    id[[i]] <<- replicate(length(ind), i) # polygon ids
+    pc[[i]] <<- r[ind] # pixel percent cover
+    return()
   }
-  
-  # check each polygon
-  ov <- lapply(1:np, lf)
-  xc <- sapply(ov, function(x) {x$x}) # x coordinates
-  yc <- sapply(ov, function(x) {x$y}) # y coordinates
-  id <- sapply(ov, function(x) {x$id}) # polygon ids
-  pc <- sapply(ov, function(x) {x$pc}) # pixel cover
-  
-  rm(rr, ov)
-
-#----------------------------------------------------------------------------------------------------------#
+  x <- sapply(1:np, lf)
+  xc <- unlist(xc)
+  yc <- unlist(yc)
+  id <- unlist(id)
+  pc <- unlist(pc)
   
   # convert coordinates to pixel indices
   sp <- (round((ras[2]-yc)/pr)+1) + dim(rr)[1] * round((xc-ras[1])/pr)
   
-  # remove duplicates (use unique pixel indices as reference)
-  # if duplicates exist, sum percent cover over repeated records
-  ui <- duplicated(sp)
-  if (sum(ui)>0) {
-    ui <- !ui
+  rm(x, rr)
+  
+#----------------------------------------------------------------------------------------------------------#
+  
+  if (ccheck) {
+    
+    # remove duplicates (use unique pixel indices as reference)
+    # if duplicates exist, sum percent cover over repeated records
+    ui <- duplicated(sp) # which are the dupllicates?
+    ud <- unique(sp[ui]) # unique duplicates
+    ui <- !ui# records to keep
+    lf <- function (i) {
+      pc[[which(ui==T & sp==i)]] <<- sum(pc[which(sp==i)])
+      return()
+    }
+    x <- sapply(ud, lf)
     xc <- xc[ui]
     yc <- yc[ui]
+    sp <- sp[ui]
     id <- id[ui]
-    pc <- apply(ind, function(x) {sum(which(sp==x))})
-    ind <- ind[ui]
+    pc <- pc[ui]
+    
+    rm(x, ud, ui)
+    
   }
-
+  
 #----------------------------------------------------------------------------------------------------------#
   
   # apply percent cover filter  
   ind <- which(pc > mpc)
   xc <- xc[ind]
   yc <- yc[ind]
+  sp <- sp[ind]
   id <- id[ind]
-  pc <- c[ind]
+  pc <- pc[ind]
   
   # build/return output
-  return(data.frame(x=xc, y=yc, index=sp, id=id, cover=pc)
+  return(data.frame(x=xc, y=yc, index=sp, id=id, cover=pc))
   
 }
