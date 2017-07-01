328ab6be8dc6e662dae3404954b548101d8358ce
Ruben Soares Lapa Remelgado <rus14jh@uni-wuerzburg.eu>
2017-07-01 20:05:43 +0200

    new functionalities

--- R/dataQuery.R
+++ R/dataQuery.R
@@ -1,85 +1,92 @@
 #' @title dataQuery
 #'
 #' @description Query environmental data for coordinate pairs.
-#' @param x Vector of x coordinates.
-#' @param y Vector of y coordinates.
-#' @param var Object of class "RasterStack" or "RasterBrick".
+#' @param xy Object of class "SpatialPoints" or "SpatialPointsDataFrame".
+#' @param ot OValid "POSIXlt" or "POSIXct" object with the same length as "xy".
+#' @param img Object of class "RasterLayer", RasterStack", "RasterBrick" or "RasterStackTS".
+#' @param type One of "exact" or "nearest".
 #' @param bs Buffer size (unit depends on the raster projection).
 #' @param remove.duplicates Logical. Default if FALSE.
 #' @param fun Passes an external function.
-#' @import raster grDevices
-#' @return Matrix of environmental variables for each sample.
+#' @import raster rts grDevices
+#' @return Matrix or vector of environmental variables for each sample.
 #' @details {Returns environmental variables from a raster object for a given set of x and y coordinates.
-#'          If "remove.duplicates" is TRUE, the function checks for and removes duplicated samples that 
-#'          fall in the same pixels. The pixel ID, contained in the output, informs on which samples were kept.
-#'          If a sample does not overlap with the reference raster NA is returned.
-#'          If "bs" is set, the function uses a buffer arround each sample and extracts a summary metric.
-#'          If "fun" is NULL, the function uses a weighted mean by default.}
+#'          A buffer size ("bs") and a user defined function ("fun") can be specified to sample within an area.
+#'          The defaut is to estimate a weighted mean. If a raster time series is provided the function applies 
+#'          one of two sampling approaches: "exact" or "nearest". If "exact", the function attempts to map the 
+#'          dates of the raster time series with the observation dates of the samples ("ot"). If nearest, 
+#'          it searches for the nearest time step.}
 #' @examples \dontrun{
 #' }
 #' @export
 
 #-------------------------------------------------------------------------------------------------------------------------------#
 
-dataQuery <- function(x=x, y=y, var=var, bs=NULL, remove.duplicates=F, fun=NULL) {
+dataQuery <- function(xy, img=img, ot=NULL, type=NULL, bs=NULL, remove.duplicates=F, fun=NULL) {
   
-  # check variables
-  if (!exists('x')) {stop('error: "x" is missing')}
-  if (!exists('y')) {stop('error: "y" is missing')}
-  if (!exists('var')) {stop('error: "var" is missing')}
-  if (class(var)!='RasterStack' & class(var)!='RasterBrick') {stop('error: "var" is not a valid raster object')}
-  if (length(x)!=length(y)) {stop('error: x and y have different lengths')}
-  if (!is.null(bs)) {if (!is.numeric(bs)) {stop('error: "bs" assigned but not numeric')}}
-  if (!is.null(fun)) {fun <- function(x) {sum(x*x) / sum(x)}} else {
+#-------------------------------------------------------------------------------------------------------------------------------#
+# check variables
+#-------------------------------------------------------------------------------------------------------------------------------#
+  
+  # samples
+  if (!exists('xy')) {stop('error: "xy" is missing')}
+  if (!class('xy')%in%c('SpatialPoints', 'SpatialPolygonsDataFrame')) {stop('error: "xy" is not of a valid class')}
+  
+  # raster
+  if (!exists('img')) {stop('error: "img" is missing')}
+  if (!class(img)[1]%in%c('RasterStack', 'RasterBrick', 'RasterStackTS')) {stop('error: "img" is not of a valid class')}
+  if (crs(xy)@projargs!=crs(img)@projargs) {stop('error: "xy" and "img" have different projections')}   
+  
+  # check if raster is a ts
+  if (class(img)[1]=='RasterStackTS') {
+    if (is.null(ot)) {stop('error: "ts" object provided. "ot" is missing')}
+    if (!class(ot)[1]%in%c('Date', 'POSIXlt'', POSIXct')) {stop('error: "ot" is not of a valid class')}
+    if (length(ot)!=length(xy)) {stop('error: lengths of "ot" and "xy" differ')}
+    processTime <- TRUE
+  } else {processTime <- FALSE}
+  
+  # auxiliary
+  if (!is.null(bs)) {if (!is.numeric(bs)) {stop('error: "bs" assigned but not numeric')}} else {fun=NULL}
+  if (!is.null(bs) & is.null(fun)) {fun <- function(x) {sum(x*x) / sum(x)}} else {
     if (!is.function(fun)) {stop('error: "fun" is not a valid function')}
     tf <- fun(c(1,2,3)) # test function
-    if (length(tf)!=1) {stop('error: provided function returns more than 1 output.')}
+    if (length(tf)!=1) {stop('error: provided function returns more than 1 output')}
   }
   
-#-------------------------------------------------------------------------------------------------------------------------------#
-  
-  # derive pixel coordinates
-  ext <- raster::extent(var) # raster extent
-  dims <- dim(var) # image dimensions
-  pxr <- raster::res(var)[1] # raster resolution
-  sp <- (round((ext[4]-y)/pxr)+1) + dims[1] * round((x-ext[1])/pxr) # convert coordinates to pixel positions
-  if (remove.duplicates) {
-    sp <- unique(sp) # return unique records
-    id <- which(!duplicate(sp)) # return indices of unique records
-  } else {
-    id <- 1:length(sp)  
-  }
-
-#-------------------------------------------------------------------------------------------------------------------------------#
-  
-  # identify pixels with valid/invalid samples
-  np <- dims[1] * dims[2] # number of pixels
-  if (max(sp) < 0 | min(sp) > np) {stop('error: samples do not overlap with the environmental data (is the projection correct?)')}
-  ind0 <- which(sp < 0 | sp > np) # pixels outside the matrix
-  ind1 <- which(sp > 0 | sp < np) # pixels inside the valid range
-
 #-------------------------------------------------------------------------------------------------------------------------------#
   
   # extract environmental data
-  ov <- matrix(0, length(sp), (raster::nlayers(var)+1))
-  ov[ind0,] <- NA
-  ov[,1] <- id
-  if (!is.null(bb)) {
-    bs <- round(bs / pxr)
-    bf <- function(x) {
-      xp <- x / dims[1]
-      yp <- x %% dims[1]
-      sr <- yp-bs
-      if (sr < 1) {sr<-1}
-      er <- yp+bs
-      if (er > dims[1]) {er=dims[1]}
-      sc <- xp-bs
-      if (sc < 1) {sc<-1}
-      ec <- xp+bs
-      if (ec > dims[2]) {ec<-dims[2]}
-      return(fun(var[sc:ec,sr:er]))}
-    ov[ind1,2:ncol(ov)] <- unlist(sapply(ind1, bf))
-  } else {ov[ind1,2:ncol(ov)] <- var[sp[ind1]]}
+  if (processTime) {
+    
+    r.dates <- index(img) # extract rts object dates
+    s.dates <- as.Date(ot) # convert sample timestamp to "Date"
+    
+    # function for exact date query
+    qf <- if (type=='exact') {
+      function(x) {
+        ind <- which(r.dates==s.dates[x])
+        if (length(ind)>1) {return(extract(img[[ind]], xy@coords[x], buffer=bs, fun=fun, na.rm=T))}
+      }
+    }
+    
+    # function for nearest date query
+    qf <- if (type=="nearest") {
+      function(x) {
+        td <- abs(s.dates[x]-r.dates[x])
+        ind <- which(td==min(td))
+        return(extract(img[[ind]], xy@coords[x], buffer=bs, fun=fun, na.rm=T))
+      }
+    }
+    
+    # apply query function
+    ov <- unlist(sapply(1:nrow(xy), qf))
+    
+  } else {
+    
+    # simple query
+    ov <- extract(img, xy@coords, buffer=bs, fun=fun, na.rm=T)
+  
+  }
   
 #-------------------------------------------------------------------------------------------------------------------------------#
   
