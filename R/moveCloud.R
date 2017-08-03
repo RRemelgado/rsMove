85b994f9ac3c79ba5af2521b70bcbcda6d426678
Ruben Soares Lapa Remelgado <rus14jh@uni-wuerzburg.eu>
2017-08-03 11:35:32 +0200

    update

---
+++ R/moveCloud.R
@@ -0,0 +1,218 @@
+#' @title moveCloud
+#'
+#' @description Provides historical information on cloud cover.
+#' @param xy Object of class \emph{SpatialPoints} or \emph{SpatialPointsDataFrame}.
+#' @param d.path Output data path for downloaded data.
+#' @param d.buffer
+#' @param remove.files
+#' @param p.res Should the output be ploted on screen? Default is TRUE.
+#' @import ggplot2 sp rgdal grDevices
+#' @importFrom utils download.file
+#' @return A \emph{list}.
+#' @details {This function makes uses daily cloud fraction data from NASA's NEO service. 
+#' For each observation date, the function downloads the correspondent image and extracts 
+#' the percent of cloud cover for the samples acquired at the target date. If \emph{d.path} 
+#' is specified, the function will look within the provided directory if the required files 
+#' are already. If so, they won't be downloaded. If \emph{d.buffer} is specified, for each 
+#' date, the function will consider images before and after within a temporal buffer. These 
+#' new images will be used to report on the closest time step with the lowest possible cloud 
+#' cover. The final report provides information on:
+#' \itemize{
+#'  \item{\emph{day.cover}: cloud cover for the observation date}
+#'  \item{\emph{p.day.before}: date before the obsertation date with the lowest cloud cover}
+#'  \item{\emph{p.cover.before}: cloud cover for \emph{p.day.before}}
+#'  \item{\emph{p.day.after}: date after the obsertation date with the lowest cloud cover}
+#'  \item{\emph{p.cover.after}: cloud cover for \emph{p.day.after}}
+#'  The output will also contain a plot provide information on the distance between the observation 
+#'  dates and the closest date with the lowest cloud cover.}
+#' @references \url{https://cneos.jpl.nasa.gov/}
+#' @seealso \code{\link{sMoveRes}}
+#' @examples \dontrun{
+#'  
+#'  require(raster)
+#'  
+#'  # read raster data
+#'  r <- raster(system.file('extdata', 'tcb_1.tif', package="rsMove"))
+#'  
+#'  # read movement data
+#'  moveData <- read.csv(system.file('extdata', 'konstanz_20130804.csv', package="rsMove"))
+#'  moveData <- SpatialPointsDataFrame(moveData[,1:2], moveData, proj4string=crs(r))
+#'  
+#'  # test function for 5, 10 20 and 30 m
+#'  a.res <- tMoveRes(xy=moveData, dpath='.')
+#'  
+#' }
+#' @export
+
+#-------------------------------------------------------------------------------------------------------------------------------#
+
+moveCloud <- function(xy=xy, d.path=NULL, b.size=NULL, remove.file=TRUE, p.res=T) {
+  
+#---------------------------------------------------------------------------------------------------------------------#
+#  1. check inpur variables
+#---------------------------------------------------------------------------------------------------------------------#
+  
+  # input keywords
+  if (!class(xy)[1]%in%c('SpatialPoints', 'SpatialPointsDataFrame')) {stop('"xy" is not of a valid class')}
+  if (is.na(rr@projargs)) {stop('"xy" does not have a valid projection')}
+  if (is.null(d.path)) {
+    d.path <- tempdir()
+    remove.file <- TRUE
+  } else {
+    if (!dir.exists(d.path)) {stop('"dpath" not found in file system')}
+    d.path <- paste0(file.path(d.path), .Platform$file.sep)}
+  if (!is.null(b.size)) {apply.buffer=TRUE}
+  if (!is.logical(p.res)) {stop('"p.res" is not a logical argument')}
+  
+  # ftp servers
+  myd <- "ftp://neoftp.sci.gsfc.nasa.gov/geotiff.float/MYDAL2_D_CLD_FR/" # aqua
+  mod <- "ftp://neoftp.sci.gsfc.nasa.gov/geotiff.float/MODAL2_D_CLD_FR/" # terra
+  
+#---------------------------------------------------------------------------------------------------------------------#
+# 2. download data and derive statistics
+#---------------------------------------------------------------------------------------------------------------------#
+  
+  # target dates
+  ot <- as.Date(ot)
+  ud <- unique(ot)
+  
+  # output variables
+  d.cc <- vector('numeric', length(xy))
+  p.cc.b <- d.cc
+  p.dt.b <- d.cc
+  class(p.dt.b) <- "Date"
+  d.df.b <- d.cc
+  p.dt.df <- d.cc
+  p.cc.a <- d.cc
+  p.dt.a <- p.dt.b
+  d.df.a <- d.cc
+  
+  for (d in 1:length(ud)) {
+    
+    # target observations
+    loc <- which(ot==ud[d])
+    
+    # set file name
+    ifile1 <- paste0(mod, "MODAL2_D_CLD_FR_", ud[d], ".FLOAT.TIFF")
+    ofile1 <- paste0(d.path, basename(ifile1))
+    ifile2 <- paste0(myd, "MYDAL2_D_CLD_FR_", ud[d], ".FLOAT.TIFF")
+    ofile2 <- paste0(d.path, basename(ifile2))
+    
+    # check if file exists
+    if (!file.exists(ofile1)) {
+      if (url.exists(ifile1)) {download.file(ifile1, ofile1, quiet=TRUE, mode="wb")
+        mod.r <- TRUE} else {mod.r <- FALSE}} else {mod.r <- TRUE}
+    if (!file.exists(ofile2)) {
+      if (url.exists(ifile2)) {download.file(ifile2, ofile2, quiet=TRUE, mode="wb")
+        myd.r <- TRUE} else {myd.r <- FALSE}} else {myd.r <- TRUE}
+    
+    # read data and crop to xy extent
+    if (mod.r & myd.r) {d.cc[loc] <- (extract(raster(ofile1), xy[loc,]) + extract(raster(ofile2), xy[loc,])) / 2}
+    if (mod.r & !myd.r) {d.cc[loc] <- extract(raster(ofile1), xy[loc,])}
+    if (!mod.r & myd.r) {d.cc[loc] <- extract(raster(ofile1), xy[loc,])}
+    
+    # search for nearby images
+    if(apply.buffer) {
+      
+      # determine dates within the buffer
+      day.ls <- seq(ud[d]-b.size, ud[d]+b.size, 1)
+      
+      df <- lapply(day.ls, function(x) {
+        ifile1 <- paste0(mod, "MODAL2_D_CLD_FR_", x, ".FLOAT.TIFF")
+        ofile1 <- paste0(d.path, basename(ifile1))
+        ifile2 <- paste0(myd, "MYDAL2_D_CLD_FR_", x, ".FLOAT.TIFF")
+        ofile2 <- paste0(d.path, basename(ifile2))
+        if (!file.exists(ofile1)) {
+          if (url.exists(ifile1)) {download.file(ifile1, ofile1, quiet=TRUE, mode="wb")
+            mod.r <- TRUE} else {mod.r <- FALSE}}
+        if (!file.exists(ofile2)) {
+          if (url.exists(ifile2)) {download.file(ifile2, ofile2, quiet=TRUE, mode="wb")
+            myd.r <- TRUE} else {myd.r <- FALSE}}
+        if (mod.r & myd.r) {return((extract(raster(ofile1), xy[loc,]) + 
+                                     extract(raster(ofile2), xy[loc,])) / 2)}
+        if (mod.r & !myd.r) {return(extract(raster(ofile1), xy[loc,]))}
+        if (!mod.r & myd.r) {return(extract(raster(ofile2), xy[loc,]))}})
+      
+      # extract values
+      f.cc <- do.call(cbind, df)
+        
+      # find closest minimum
+      dq <- lapply(1:length(loc), function(x) {
+        diff0 <- day.ls - ud[d]
+        ind <- which(diff0 < 0)
+        bv <- min(f.cc[x,ind])
+        diff1 <- abs(diff0[ind])
+        ind <- ind[which(f.cc[x,ind]==av)]
+        ind <- ind[which(diff0[ind]==min(diff0[ind]))]
+        db <- diff0[ind]
+        bd <- day.ls[ind]
+        ind <- which(diff0 > 0)
+        bv <- min(f.cc[x,ind])
+        diff1 <- abs(diff0[ind])
+        ind <- ind[which(f.cc[x,ind]==av)]
+        ind <- ind[which(diff0[ind]==min(diff0[ind]))]
+        da <- abs(day.ls[ind]-ud[d])
+        ad <- day.ls[ind]
+        return(list(bd=bd, bv=bv, db=db, ad=ad, av=av, da=da))})
+      
+      # update target variables
+      p.dt.b[loc] <- do.call('c', lapply(dq, function(x) {x$bd}))
+      p.cc.b[loc] <- unlist(lapply(dq, function(x) {x$bv}))
+      d.df.b[loc] <- unlist(lapply(dq, function(x) {x$db}))
+      p.dt.a[loc] <- do.call('c', lapply(dq, function(x) {x$ad}))
+      p.cc.a[loc] <- unlist(lapply(dq, function(x) {x$av}))
+      d.df.a[loc] <- unlist(lapply(dq, function(x) {x$da}))
+      
+      rm(f.cc)
+      
+    }
+    
+    # remove files if required
+    if (remove.file) {file.remove(list.files(d.path, '_D_CLD_FR_'))}
+    
+  }
+  
+  # add column names output
+  df <- data.frame(day.cover=d.cc, p.cover.before=p.cc.b, p.cover.after=p.cc.a, 
+                   p.day.before=p.dt.b, p.day.after=p.dt.a, stringsAsFactors=F)
+  
+#-------------------------------------------------------------------------------------------#
+# 3. build plot
+#-------------------------------------------------------------------------------------------#
+  
+  # make color ramp
+  cr = colorRampPalette(c("forestgreen", "khaki2", "darkred"))
+  
+  # data frame used for plotting
+  odf <- data.frame(dd=c(d.df.b, d.df.a), 
+                    cc=c(p.cc.b, p.cc.a))
+  
+  # build plot
+  p <- ggplot(odf, aes(x=dd, fill=cc)) + geom_histogram(binwidth=1) + 
+    scale_x_continuous(limits=c(-b.size, b.size)) + 
+    scale_fill_gradientn(name='Cloud cover %', colors=cr(10), limits=c(0,100), breaks=c(0, 50, 100)) +
+    xlab("Day Difference") + ylab("Number of Samples") + 
+    theme(axis.text=element_text(size=10), 
+          axis.title=element_text(size=12))
+  
+  # determine optimal y range
+  mv <- max(ggplot_build(p)$data[[1]]$count)
+  nc <- nchar(as.character(mv))
+  m <- as.numeric(paste0(1, paste0(replicate((nc-1), '0'), collapse='')))
+  mv <- mv / m
+  yr <- round(mv)
+  if (mv > yr) {yr <- (yr+0.05)*m} else {yr <- yr*m}
+  
+  # update plot
+  p <- p + scale_y_continuous(limits=c(0, yr))
+  
+  if (p.res) {p} # plot raster on screen
+  
+  #---------------------------------------------------------------------------------------------------------------------#
+  #  7. derive output
+  #---------------------------------------------------------------------------------------------------------------------#
+  
+  # return data frame and plot
+  return(list(stats=df, plot.data=odf, plot=p))
+  
+}
--- R/tMoveRes.R
+++ R/tMoveRes.R
@@ -2,23 +2,17 @@
 #'
 #' @description Provides historical information on cloud cover.
 #' @param xy Object of class \emph{SpatialPoints} or \emph{SpatialPointsDataFrame}.
-#' @param dpath Output data path for downloaded data.
+#' @param t.res Temporal resolution.
+#' @param s.res Spatial resolution.
 #' @param p.res Should the output be ploted on screen? Default is TRUE.
 #' @import ggplot2 sp rgdal grDevices
 #' @importFrom utils download.file
 #' @return A \emph{list}.
-#' @details {This function makes use of the global cloud cover statistics provided 
-#' through EarthEnv (\url{http://www.earthenv.org/cloud}) to extract base statistics 
-#' for a shapefile containing GPS tracking data. The function starts by downloading 
-#' monthly means of fractional cloud cover. This requires 9.63 Gb of space on the disk. 
-#' If the data already exists in \emph{dpath} this step will ignored. If the data 
-#' already exists, please assure it has not been modified. Then, the values for each 
-#' point are extracted (\emph{$point.data}) and used to derive the min, max, mean and 
-#' sd for each month (\emph{$stats}). This statistics are then used to build a plot 
-#' with ggplot expressing the mean and maximum cloud cover fraction per month. This 
-#' plot will be added to the output (\emph{$plot}). If \emph{p.res} is TRUE, it is 
-#' also ploted on screen.}
-#' @references \url{https://doi.org/10.1371/journal.pbio.1002415}
+#' @details {Given a vector of temporal resolutions (\emph{t.res}), the function determines 
+#' the number of unique pixels and unique pixel groups after their remporal agggation. The 
+#' function returns the corresponding pixel indices per resolution showing which 
+#' samples would be grouped (\emph{$indices}). The function returns a data frame (\emph{$stats}) 
+#' and a plot (\emph{$plot}) with the statistics per temporal resolution.}
 #' @seealso \code{\link{sMoveRes}}
 #' @examples \dontrun{
 #'  
@@ -39,7 +33,7 @@
 
 #-------------------------------------------------------------------------------------------------------------------------------#
 
-tMoveRes <- function(xy=xy, dpath=dpath, p.res=T) {
+tMoveRes <- function(xy=xy, o.time=o.time, t.res=t.res, s.res=s.res, p.res=T) {
   
 #---------------------------------------------------------------------------------------------------------------------#
 #  1. check inpur variables
@@ -48,79 +42,138 @@ tMoveRes <- function(xy=xy, dpath=dpath, p.res=T) {
   if (!class(xy)[1]%in%c('SpatialPoints', 'SpatialPointsDataFrame')) {stop('"xy" is not of a valid class')}
   rr <- crs(xy) # reference projection
   if (is.na(rr@projargs)) {stop('"xy" does not have a valid projection')}
-  if (!dir.exists(dpath)) {stop('"dpath" not found in file system')}
+  if (length(s.res)>1) {stop('"s.res" has more than one element')}
   if (!is.logical(p.res)) {stop('"p.res" is not a logical argument')}
   
 #---------------------------------------------------------------------------------------------------------------------#
-#  2. list, check for and download files
+# 2. determine grid coordinates for given pixels
 #---------------------------------------------------------------------------------------------------------------------#
   
-  # target files
-  i.files <- c(paste0('http://data.earthenv.org/cloud/MODCF_monthlymean_', sprintf('%02d', 1:12), '.tif'))
-  o.files <- file.path(dpath, basename(i.files))
+  ext <- extent(xy) # reference extent
+  nc <- round((ext[2]-ext[1]) / s.res) + 1 # number of columns
+  nr <- round((ext[4]-ext[3]) / s.res) + 1 # number of rows
+  sp <- (round((ext[4]-xy@coords[,2])/s.res)+1) + nr * round((xy@coords[,1]-ext[1])/s.res) # convert coordinates to pixel positions
   
-  # download missing files
-  cc <- file.exists(o.files)
-  if (sum(!cc)>0) {
-    print('downloading files')
-    ind <- which(!cc)
-    for (f in 1:length(ind)) {download.file(i.files[ind[f]], o.files[ind[f]], mode="wb")}}
+#---------------------------------------------------------------------------------------------------------------------#
+# 3. find unique sample regions
+#---------------------------------------------------------------------------------------------------------------------#
 
+  up <- unique(sp) # unique pixel positions
+  
+  # evaluate pixel connectivity
+  regions <- matrix(0, nr, nc)
+  for (r in 1:length(up)) {
+    rp <- ((up[r]-1) %% nr)+1
+    cp <- ((up[r]-1) %/% nr)+1
+    if (cp > 1) {sc<-cp-1} else {sc<-cp}
+    if (cp < nc) {ec<-cp+1} else {ec<-cp}
+    if (rp > 1) {sr<-rp-1} else {sr<-rp}
+    if (rp < nr) {er<-rp+1} else {er<-rp}
+    if (max(regions[sr:er,sc:ec])>0) {
+      uv <- unique(regions[sr:er,sc:ec])
+      uv <- uv[which(uv > 0)]
+      mv <- min(uv)
+      regions[rp,cp] <- mv
+      for (u in 1:length(uv)) {regions[which(regions==uv[u])] <- mv}
+    } else {regions[rp,cp] <- max(regions)+1}
+  }  
+  
+  # update region ID's
+  uregions <- sapply(up, function(x) {regions[x]})
+  
+  rm(regions)
+  
 #---------------------------------------------------------------------------------------------------------------------#
-#  3. read, crop and scale data
-#---------------------------------------------------------------------------------------------------------------------#
-  # column names for extracted values
-  cn <- c('Jan', 'Feb', 'Mar', 'Apr', 
-          'May', 'Jun', 'Jul', 'Aug', 
-          'Sep', 'Oct', 'Nov', 'Dec')
-
-  # read and crop image stack
-  c.data <- lapply(o.files, function(x){extract(raster(x), xy)*0.01})
-  c.data <- do.call(cbind, lapply(c.data, data.frame))
-  colnames(c.data) <- cn
-
-#---------------------------------------------------------------------------------------------------------------------#
-#  4. derive statistics
+# 4. determine pixel aggregations
 #---------------------------------------------------------------------------------------------------------------------#
   
-  # estimate base area statistics
-  mnv <- apply(c.data, 2, min, na.rm=T)
-  mxv <- apply(c.data, 2, max, na.rm=T)
-  avv <- apply(c.data, 2, mean, na.rm=T)
-  sdv <- apply(c.data, 2, sd, na.rm=T)
+  st <- min(o.time) # start time
+  et <- max(o.time) # end time
   
-  # aggregate stats in common data frame (used for output)
-  odf <- data.frame(month=cn, min=mnv, max=mxv, mean=avv, sd=sdv, stringsAsFactors=F)
+  id <- 0 # reference sample ID
+  out <- list() # output variable
+  for (r in 1:length(t.res)) {
+    
+    ind <- vector('numeric', length(xy)) # position index
+    nw <- as.numeric(((et - st) / t.res[r]) + 1) # number of temporal windows
+    nr <- vector('numeric', length(nw)) # number of regions
+    
+    for (w in 1:length(nw)) {
+      
+      # locate pixels within the temporal window
+      loc1 <- which(o.time >= (st+(t.res[r]*(w-1))) & 
+                     o.time <= ((st+t.res[r])+(t.res[r]*(w-1))))
+       
+      # quantify unique samples
+      upr <- unique(sp[loc1])
+      for (p in 1:length(upr)) {
+        id <- id + 1
+        loc2 <- which(sp[loc1]==upr[p])
+        ind[loc1[loc2]] <- id}
+      
+      # number of unique regions
+      nr[w] <- length(unique(uregions[up%in%upr]))}
+    
+    # update output
+    out[[r]] <- list(count=max(ind), indices=ind, regions=sum(nr))
+    
+  }
   
-  rm(mnv, mxv, avv, sdv)
-
-#---------------------------------------------------------------------------------------------------------------------#
-#  5. build plot
-#---------------------------------------------------------------------------------------------------------------------#
+  # output data frame with statistics
+  out1 <- data.frame(n.pixels=sapply(out, function(x) {x$count}), 
+                     n.regions=sapply(out, function(x) {x$regions}))
+  row.names(out1) <- as.character(pxr)
   
-  # make color ramp
-  cr = colorRampPalette(c("forestgreen", "khaki2", "darkred"))
+  # output data frame with sample indices
+  out <- lapply(out, function(x) {x$indices})
+  out2 <- do.call(cbind, lapply(out, data.frame, stringsAsFactors=FALSE))
+  colnames(out2) <- as.character(pxr)
   
-  # plot
-  p <- ggplot(odf, aes(x=factor(month, levels=cn), y=mean, fill=max)) + 
-    scale_fill_gradientn(colors=cr(10), breaks=c(0, 50, 100), 
-                         limits=c(0,100), name="Max. %\n") + 
-    xlab("\nMonth") + ylab("Mean Cloud Cover (%)\n") + ylim(0,100) + 
-    geom_bar(width=0.7, stat="identity") + 
+  #---------------------------------------------------------------------------------------------------------------------#
+  # 4. plot output
+  #---------------------------------------------------------------------------------------------------------------------#
+  
+  # determine fill scale range
+  mv = max(out1$n.regions)
+  if (mv < 100) {
+    mv <- mv / 10
+    fr <- round(mv*2)/2
+    if (mv > fr) {fr <- (fr+0.5)*10} else {fr <- fr*10}
+  }
+  if (mv >= 100) {
+    mv <- mv / 100
+    fr <- round(mv*20)/20
+    if (mv > fr) {fr <- (fr+0.5)*100} else {fr <- fr*100}}
+  
+  # determine yscale range
+  mv <- max(out1$n.pixels)
+  if (mv < 100) {
+    mv <- mv / 10
+    yr <- round(mv*2)/2
+    if (mv > yr) {yr <- (yr+0.5)*10} else {yr <- yr*10}}
+  if (mv >= 100) {
+    mv <- mv / 100
+    yr <- round(mv*20)/20
+    if (mv > yr) {yr <- (yr+0.5)*100} else {yr <- yr*100}}
+  
+  # make color palette
+  cr <- colorRampPalette(c("khaki2", "forestgreen"))
+  
+  # build plot object
+  p <- ggplot(out1, aes(x=factor(pxr), y=n.pixels, fill=n.regions)) + 
+    scale_fill_gradientn(colors=cr(10), breaks=c(0.0, (fr/2), fr), 
+                         limits=c(0,fr), name="Nr. Regions\n") + xlab("\nResolution") + 
+    ylab("Nr. Pixels\n") + geom_bar(width=0.7, stat = "identity") + 
     theme(axis.text.x=element_text(size=12), 
           axis.title.x =element_text(size=14), 
           axis.text.y=element_text(size=12),
           axis.title.y =element_text(size=14),
           legend.text=element_text(size=12), 
-          legend.title=element_text(size=14))
+          legend.title=element_text(size=14)) + ylim(0,yr)
   
-  if (p.res) {p} # plot raster on screen
-    
-#---------------------------------------------------------------------------------------------------------------------#
-#  7. derive output
-#---------------------------------------------------------------------------------------------------------------------#
-
+  if (p.res) {p} # plot on screen
+  
   # return data frame and plot
-  return(list(stats=odf, point.data=c.data, plot=p))
+  return(list(stats=out1, plot=p, indices=out2))
   
 }
