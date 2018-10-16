edd4fce497a4f9bafdba905661c85e57cb2afb41
Ruben Soares Lapa Remelgado <rus14jh@uni-wuerzburg.eu>
2018-10-16 22:44:22 +0200

    update

--- vignettes/resource_suitability.R
+++ vignettes/resource_suitability.R
@@ -27,14 +27,8 @@ move.mask <- reduced.samples$total.time > 0 & reduced.samples$total.time < upper
 usable.pixels <- which.max(move.mask) # identify relevant pixels
 presence.samples <- SpatialPoints(xyFromCell(move.mask, usable.pixels), proj4string=crs(shortMove)) # build shapefile from samples (presences)
 
-## ----eval=FALSE----------------------------------------------------------
-#  sample.id <- labelSample(presence.samples, ndvi, agg.radius=60) # aggregate samples in space
-
-## ----echo=FALSE, message=FALSE-------------------------------------------
-sample.id <- read.table(list.files(system.file('extdata', '', package="rsMove"), 'sampleIndices.txt', full.names=TRUE))[,,1]
-
 ## ----message=FALSE-------------------------------------------------------
-sample.id # show indices
+sample.id <- labelSample(presence.samples, ndvi, agg.radius=60) # aggregate samples in space
 absence.samples <- backSample(presence.samples, ndvi, sample.id, sampling.method="pca") # identify absence samples
 absence.samples # show samples
 
@@ -46,29 +40,4 @@ resourceModel1 <- predictResources(env.presences, env.absences, sample.id, env.d
 ## ---- out.width="98%", fig.height=5, fig.width=10, dpi=600, fig.align="center"----
 plot(resourceModel1$probabilities >= 0.5) # probability map
 points(presence.samples) # presences
-
-## ---- out.width="98%", fig.height=5, fig.width=10, dpi=600, fig.align="center", echo=FALSE----
-kable_styling(kable(head(resourceModel1$f1, 2), format="html", align="c", full_width=TRUE), "stripped", bootstrap_options="responsive")
-
-## ----message=FALSE, warning=FALSE, results='hide'------------------------
-absence.samples <- backSample(presence.samples, ndvi, sampling.method="random") # identify absence samples (random)
-env.absences <- extract(ndvi, absence.samples)  # extract environmental data for absences
-resourceModel2 <- predictResources(env.presences, env.absences, sample.id, env.data=ndvi) # build model
-
-## ---- out.width="98%", fig.height=5, fig.width=10, dpi=600, fig.align="center"----
-plot(resourceModel2$probabilities >= 0.5) # probability map
-points(presence.samples) # presences
-kable_styling(kable(head(resourceModel2$f1, 2), format="html", align="c", full_width=TRUE), "stripped", bootstrap_options="responsive")
-
-## ----message=FALSE-------------------------------------------------------
-landCover <- raster(system.file('extdata', 'landCover.tif', package="rsMove"))
-
-## ----message=TRUE--------------------------------------------------------
-class.labels <- c("Arable land", "Land without use", "Open spaces", "Wetlands", "Permanent crops", "Extraction/Dump sites", "Industrial areas", "Green urban areas")
-probMask <- stack(resourceModel1$probabilities> 0.5, resourceModel2$probabilities> 0.5) # stack of probabilities (pca and random)
-ptest <- plausibilityTest(probMask, landCover, class.labels=class.labels)
-
-## ---- out.width="98%", fig.height=5, fig.width=10, dpi=600, fig.align="center", echo=FALSE----
-ptest$relative.plot
-kable_styling(kable(head(ptest$relative.count, 8), format="html", align="c", full_width=TRUE), "stripped", bootstrap_options="responsive")
 
