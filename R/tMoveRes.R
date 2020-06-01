037494d57f52e2bad99e10b7a047bd32a47eaece
rr70wedu <ruben.remelgado@idiv.de>
2020-06-01 19:04:03 +0200

    example change

--- R/tMoveRes.R
+++ R/tMoveRes.R
@@ -28,7 +28,7 @@
 #'
 #'  # test function for intervals of 1, 8 and 16 days (e.g. of MODIS data)
 #'  obs.date <- as.Date(longMove@data$timestamp)
-#'  a.res <- tMoveRes(longMove, obs.date, c(1,8,16), 0.01)
+#'  a.res <- tMoveRes(longMove, obs.date, c(1,8,16), 0.1)
 #'
 #' }
 #' @export
